13b1cbe29a0850b75cde1cd0f75e02ceb5bc27b9
JoaoVGStahl <jvitorstahl@hotmail.com>
2021-10-20 22:24:20 -0300

    insert e listagem subcategoria

--- admin/subcategorias/salvar.php
+++ admin/subcategorias/salvar.php
@@ -1,11 +1,12 @@
 <?php
     $categoria = $_POST["categoria"];
+    $subcategoria = $_POST["subcategoria"];
     include("../../includes/conexao.php");
 
-    $gravar = mysqli_query($conexao, "insert into tb_categorias VALUES(0, '$categoria')");
+    $gravar = mysqli_query($conexao, "insert into tb_subcategorias VALUES(0, '$categoria','$subcategoria')");
     if ($gravar){
-        echo "<script>alert('Categoria gravada com sucesso!!')</script>" ;
-        echo '<meta http-equiv="refresh" content="0;url=../admin.php?pg=Categorias">';
+        echo "<script>alert('Subcategoria gravada com sucesso!!')</script>" ;
+        echo '<meta http-equiv="refresh" content="0;url=../admin.php?pg=SubCategorias">';
     }else{
         echo "ERRO!!"; 
     }
